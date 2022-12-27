6c971c75c1b2cefc5c8762ae8f0ba79f4e4bdabb
Kostiantyn <cashalot80@ukr.net>
2022-12-27 11:43:46 +0000

    Add logo

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -1,7 +1,14 @@
-
-
+// import { Link } from 'react-router-dom';
+import logoMain from '../../../images/logo/EvOl.png';
+import { Logo } from './Logo.styled';
 export const LogoApp = () => {
-    return (
-        
-    )
-}
+  const takeLogo = () => {
+    return logoMain;
+  };
+
+  return (
+    <>
+      <Logo src={takeLogo()} />
+    </>
+  );
+};
